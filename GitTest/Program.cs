2c8328c4e35f2795392cf636a5e8bb43877b58f4
wuzifei <wuzifei@hotmail.com>
2014-06-05 16:10:53 +0800

    fistCommit

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("This is a Git Console App !");
+            Console.ReadKey();
         }
     }
 }
